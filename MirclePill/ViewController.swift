5e6b605a0d11522018533b0c0ca988d8f5dbf3e9
toanhtran <toanh.t.tran@gmail.com>
2016-10-19 13:52:27 +0900

    add buyNowBtn action

--- MirclePill/ViewController.swift
+++ MirclePill/ViewController.swift
@@ -9,6 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var successImage: UIImageView!
+    
+    let states = [ "AK - Alaska",
+                   "AL - Alabama",
+                   "AR - Arkansas",
+                   "AS - American Samoa",
+                   "AZ - Arizona",
+                   "CA - California",
+                   "CO - Colorado",
+                   "CT - Connecticut",
+                   "DC - District of Columbia",
+                   "DE - Delaware",
+                   "FL - Florida",
+                   "GA - Georgia",
+                   "GU - Guam",
+                   "HI - Hawaii",
+                   "IA - Iowa",
+                   "ID - Idaho",
+                   "IL - Illinois",
+                   "IN - Indiana",
+                   "KS - Kansas",
+                   "KY - Kentucky",
+                   "LA - Louisiana",
+                   "MA - Massachusetts",
+                   "MD - Maryland",
+                   "ME - Maine",
+                   "MI - Michigan",
+                   "MN - Minnesota",
+                   "MO - Missouri",
+                   "MS - Mississippi",
+                   "MT - Montana",
+                   "NC - North Carolina",
+                   "ND - North Dakota",
+                   "NE - Nebraska",
+                   "NH - New Hampshire",
+                   "NJ - New Jersey",
+                   "NM - New Mexico",
+                   "NV - Nevada",
+                   "NY - New York",
+                   "OH - Ohio",
+                   "OK - Oklahoma",
+                   "OR - Oregon",
+                   "PA - Pennsylvania",
+                   "PR - Puerto Rico",
+                   "RI - Rhode Island",
+                   "SC - South Carolina",
+                   "SD - South Dakota",
+                   "TN - Tennessee",
+                   "TX - Texas",
+                   "UT - Utah",
+                   "VA - Virginia",
+                   "VI - Virgin Islands",
+                   "VT - Vermont",
+                   "WA - Washington",
+                   "WI - Wisconsin",
+                   "WV - West Virginia",
+                   "WY - Wyoming"]
+    
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,67 +79,8 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
         statePicker.delegate = self
     }
 
-    @IBOutlet weak var sucessBtn: UIButton!
-    @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var statePickerBtn: UIButton!
     
-    let states = [ "AK - Alaska",
-                  "AL - Alabama",
-                  "AR - Arkansas",
-                  "AS - American Samoa",
-                  "AZ - Arizona",
-                  "CA - California",
-                  "CO - Colorado",
-                  "CT - Connecticut",
-                  "DC - District of Columbia",
-                  "DE - Delaware",
-                  "FL - Florida",
-                  "GA - Georgia",
-                  "GU - Guam",
-                  "HI - Hawaii",
-                  "IA - Iowa",
-                  "ID - Idaho",
-                  "IL - Illinois",
-                  "IN - Indiana",
-                  "KS - Kansas",
-                  "KY - Kentucky",
-                  "LA - Louisiana",
-                  "MA - Massachusetts",
-                  "MD - Maryland",
-                  "ME - Maine",
-                  "MI - Michigan",
-                  "MN - Minnesota",
-                  "MO - Missouri",
-                  "MS - Mississippi",
-                  "MT - Montana",
-                  "NC - North Carolina",
-                  "ND - North Dakota",
-                  "NE - Nebraska",
-                  "NH - New Hampshire",
-                  "NJ - New Jersey",
-                  "NM - New Mexico",
-                  "NV - Nevada",
-                  "NY - New York",
-                  "OH - Ohio",
-                  "OK - Oklahoma",
-                  "OR - Oregon",
-                  "PA - Pennsylvania",
-                  "PR - Puerto Rico",
-                  "RI - Rhode Island",
-                  "SC - South Carolina",
-                  "SD - South Dakota",
-                  "TN - Tennessee",
-                  "TX - Texas",
-                  "UT - Utah",
-                  "VA - Virginia",
-                  "VI - Virgin Islands",
-                  "VT - Vermont",
-                  "WA - Washington",
-                  "WI - Wisconsin",
-                  "WV - West Virginia", 
-                  "WY - Wyoming"]
-    
-    override func didReceiveMemoryWarning() {
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -102,5 +106,14 @@ class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDeleg
         statePickerBtn.setTitle(states[row], for: UIControlState.normal)
         statePicker.isHidden = true
     }
+    
+    @IBAction func buyNowBtn(_ sender: AnyObject) {
+        for i in 1...14 {
+            self.view.viewWithTag(i)?.isHidden = true
+        }
+        
+    }
+   
+   
 }
 
